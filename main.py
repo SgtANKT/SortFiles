5cb915bf25799eb3e5d83c710696f32634fb396d
SgtANKT <ankitpratihast11@gmail.com>
2021-04-21 18:20:58 +0530

    added logging and deleting folders that are empty future---- will add a way to search through different directories for different files

--- main.py
+++ main.py
@@ -1,88 +1,92 @@
 import os
 import shutil
 import json
+import logging
+import datetime
 
 
 class FileHandle:
+    today_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    logging.basicConfig(filename=f"logs\\file_sort_{today_date}.txt", format='%(asctime)s:%(levelname)s:%(message)s',
+                        datefmt="%H:%M:%S", level=logging.INFO)
+    logging.info('Log file initiated')
+
+    # logging.log('Log file initiated')
     def __init__(self):
-        self.home_path = os.environ.get("USERPROFILE")
+        self.empty_folders = []
+        # self.home_path = os.environ.get("USERPROFILE")
+        pass
 
     def get_config(self):
         with open("config.json") as conf_file:
             config = json.load(conf_file)
         return config
-    
-    
+
+    def remove_dirs(self, path):
+        return shutil.rmtree(path)
+
     def find_files(self, path):
         for i in os.listdir(path):
             info = os.stat(os.path.join(path, i))
-            # print(info)
             for roots, dirs, files in os.walk(os.path.join(path, i), topdown=False):
-                print("Current Directory: ", roots)
-                # print("Sub Directories Inside Current Directory: ", dirs)
-                print("Files Inside Current Directory: ", files)
-                # return "Current File: ", roots, "Sub Directories: ", dirs, "Current File: ", files
-    
-    def organize(self):
-        conf = self.get_config()
-        dir_path = conf["Folder_Path"]
+                # print(os.path.join(path,i,roots))
+                # print(roots)
+                logging.info("Root Dirs"+roots)
+                if len(os.listdir(os.path.join(path, i, roots))) >=1:
+                    logging.info(f"Directory {os.path.join(path, i, roots)} wont be deleted as the folder has files")
+                else:
+                    logging.warning(f"Directory {os.path.join(path, i, roots)} will be deleted as there are no files in it")
+                    self.remove_dirs(os.path.join(path, i, roots))
+                # print("Directories Inside Current Directory: ", roots)
+                # logging.info("Directory"+str(dirs))
+                logging.info("Files inside directory"+str(files))
+                # logging.log(logging.INFO, files)
+                # print("files
+                # Inside Current Directory: ", files)
+                # return roots, dirs, files
+
+    def organize(self, dir_path):
         count = 1
-      
-        for i in os.listdir(os.path.join(self.home_path, dir_path)):
-            
-            if os.path.isdir(os.path.join(self.home_path, dir_path, i)):
+        for i in os.listdir(dir_path):
+
+            if os.path.isdir(os.path.join(dir_path, i)):
                 pass
-            elif os.path.isfile(os.path.join(self.home_path, dir_path, i)):
+            elif os.path.isfile(os.path.join(dir_path, i)):
                 file1 = i.split(".")
                 print(file1[-1])
                 try:
-                    new_dir = os.path.join(self.home_path, dir_path, file1[-1].upper())
+                    logging.info('')
+                    new_dir = os.path.join(dir_path, file1[-1].upper())
                     if os.path.exists(new_dir):
-                        shutil.move(os.path.join(self.home_path, dir_path, i), new_dir)
+                        shutil.move(os.path.join(dir_path, i), new_dir)
                     else:
                         os.makedirs(new_dir)
-                        shutil.move(os.path.join(self.home_path, dir_path, i), new_dir)
-                    
+                        shutil.move(os.path.join(dir_path, i), new_dir)
                 except:
-                    new_dir = os.path.join(self.home_path, dir_path, file1[-1].upper(), '('+str(count)+')')
+                    new_dir = os.path.join(dir_path, file1[-1].upper(), '(' + str(count) + ')')
                     if os.path.exists(new_dir):
-                        shutil.move(os.path.join(self.home_path, dir_path, i), new_dir)
+                        shutil.move(os.path.join(dir_path, i), new_dir)
                     else:
-                        os.makedirs(self.home_path, new_dir)
-                        shutil.move(os.path.join(self.home_path, dir_path, i), new_dir)
-                    
-                    count+=1
-        print("Your files have been organized")
-        command = input("Do you want to see how is your data sorted? (Yes/No) ")
-        if command.upper() == "YES":
-            self.find_files(os.path.join(self.home_path, dir_path))
-        else:
-            pass
-            
-            
-    def remove_dirs(self):
+                        os.makedirs(new_dir)
+                        shutil.move(os.path.join(dir_path, i), new_dir)
+
+                    count += 1
+
+
+
+
+    def main(self):
         conf = self.get_config()
-        drive = conf["Servers"]
-        paths = conf["Folders"]
-        # a = [shutil.rmtree(os.path.join(v["drive_name", value, i])) for k, v in drive.items() if v["running"] == True for key, value in paths.items() for i in os.listdir(os.path.join(v["drive_name"], value))]
-        # print(a)
-        # quit()
-        for k,v in drive.items():
-            if v["running"] == True:
-                for key, value in paths.items():
-                    dir = os.listdir(os.path.join(v["drive_name"], value))
-                    for i in dir:
-                        if os.path.isdir(os.path.join(v["drive_name"],value, i)):
-                            # print("======================================"'\n'"Folder"'\n',i)
-                            shutil.rmtree(os.path.join(v["drive_name"],value, i))
-                        else:
-                            # print("======================================"'\n'"File"'\n',i)
-                            os.remove(os.path.join(v["drive_name"],value, i))
-                print(f"Files Removed for folder {v}")
+        print("Sorting files")
+        dir_path = conf["Folder_Path"]
+        self.find_files(dir_path)
+        print('Deleting empty folders')
+        self.organize(dir_path)
+        logging.info("Your files have been organized")
 
 
 
-
-
-obj = FileHandle()
-obj.organize()
+if __name__ == '__main__':
+    obj = FileHandle()
+    obj.main()
